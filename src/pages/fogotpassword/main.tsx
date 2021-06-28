9e950e097dd9325758d06d6d4bab1aee0288b02e
arkhurst <fiiarkhurst@gmail.com>
2021-06-28 00:35:55 +0000

    added responsivenes on modal

--- src/pages/fogotpassword/main.tsx
+++ src/pages/fogotpassword/main.tsx
@@ -1,16 +1,24 @@
-import { Fragment } from "react";
+import { Fragment, useState } from "react";
 import { ForgotPasswordComponentProp } from "./types";
 import { XIcon } from "@heroicons/react/outline";
+import { useMediaQuery } from "react-responsive";
 import Modal from "../../components/modal/basic";
-import ForgotPasswordSvg from "../../components/svgs/forgotpassword";
+import SendCodeComponent from "./send-code";
+import VerifyCodeComponent from "./verify";
 
 const MainComponent: React.FC<ForgotPasswordComponentProp> = ({
   setShow,
   show,
 }) => {
+  const [tab, setTab] = useState<"SEND_CODE" | "VERIFY_CODE">("SEND_CODE");
+
+  const isTabletOrMobile = useMediaQuery({
+    query: "(min-width: 320px) and (max-width: 480px)",
+  });
+
   return (
     <Fragment>
-      <Modal show={show} setShow={setShow}>
+      <Modal show={show} setShow={setShow} size={isTabletOrMobile ? 100 : 30}>
         <div
           className={
             "w-full border-b shadow-none border-gray-200 flex flex-row justify-between px-5 pt-5 pb-3 items-center"
@@ -24,50 +32,60 @@ const MainComponent: React.FC<ForgotPasswordComponentProp> = ({
         </div>
 
         <div className={"bg-white px-20 grid grid-cols-2 shadow-none"}>
-          <div className="col-span-2">
-            <ForgotPasswordSvg className=" w-full h-48" />
-          </div>
-          <div className="col-span-2">
-            <h2 className="text-xl font-bold">Forgot Your Password ?</h2>
-          </div>
-          <div className="col-span-2 ">
-            <div className="text-center text-sm font-light pt-2">
-              {" "}
-              We get it, stuff happens. Just enter your email address below and
-              we'll send you a link to reset your password!
-            </div>
-          </div>
-          <div className="col-span-2 py-5">
-            <input
-              id="email"
-              name="email"
-              type="email"
-              autoComplete="email"
-              placeholder="Eg. johndoe@something.com"
-              required
-              className="appearance-none block bg-gray-100 w-full px-3 py-3 border-none rounded-none shadow-sm placeholder-gray-400 focus:outline-none focus:ring-white focus:border-white sm:text-sm"
-            />
-          </div>
+          {tab === "SEND_CODE" && (
+            <Fragment>
+              <SendCodeComponent />
+            </Fragment>
+          )}
+          {tab === "VERIFY_CODE" && (
+            <Fragment>
+              <VerifyCodeComponent />
+            </Fragment>
+          )}
         </div>
 
         <div className="grid grid-cols-2 border-t cursor-pointer bg-white shadow-none">
-          <button
-            onClick={() => setShow(false)}
-            className={
-              "border-r col-span-1 flex justify-center p-5 hover:bg-gray-100 "
-            }
-          >
-            <span className={"text-lg font-light"}>Close</span>
-          </button>
+          {tab === "SEND_CODE" && (
+            <Fragment>
+              <button
+                onClick={() => setShow(false)}
+                className={
+                  "border-r col-span-1 flex justify-center p-5 hover:bg-gray-100 focus:outline-none  "
+                }
+              >
+                <span className={"text-lg font-light"}>Close</span>
+              </button>
 
-          <button
-            onClick={() => setShow(false)}
-            className={
-              "border-r  col-span-1 flex justify-center p-5 hover:bg-gray-100 "
-            }
-          >
-            <span className={"text-lg font-light"}>Send Code</span>
-          </button>
+              <button
+                onClick={() => setTab("VERIFY_CODE")}
+                className={
+                  "border-r  col-span-1 flex justify-center p-5 hover:bg-gray-100 focus:outline-none  "
+                }
+              >
+                <span className={"text-lg font-light"}>Send Code</span>
+              </button>
+            </Fragment>
+          )}
+          {tab === "VERIFY_CODE" && (
+            <Fragment>
+              <button
+                onClick={() => setShow(false)}
+                className={
+                  "border-r col-span-1 flex justify-center p-5 hover:bg-gray-100 focus:outline-none "
+                }
+              >
+                <span className={"text-lg font-light"}>Close</span>
+              </button>
+
+              <button
+                className={
+                  "border-r  col-span-1 flex justify-center p-5 hover:bg-gray-100 focus:outline-none  "
+                }
+              >
+                <span className={"text-lg font-light"}>Verify Code</span>
+              </button>
+            </Fragment>
+          )}
         </div>
       </Modal>
     </Fragment>
