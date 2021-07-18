580b419ab3a1a410ed0401b5161f76384abb681e
arkhurst <fiiarkhurst@gmail.com>
2021-07-18 23:16:43 +0000

    doing somethings

--- src/pages/client-dashboard/history/main/main.tsx
+++ src/pages/client-dashboard/history/main/main.tsx
@@ -1,12 +1,13 @@
 import { Fragment } from "react";
 import Header from "../../../../shared/layout";
+import DataView from "../data-view";
 
 const MainComponent = () => {
   return (
     <Fragment>
       <Header />
-      <div className="max-w-7xl mx-auto flex justify-between items-center px-4 py-5 sm:px-6 sm:py-4 lg:px-8 md:justify-start md:space-x-10">
-        <div>hi dashboard</div>
+      <div className="max-w-7xl mx-auto px-4 py-5 sm:px-6 sm:py-4 lg:px-8  md:space-x-10">
+        <DataView />
       </div>
     </Fragment>
   );
--- src/services/context/settings.tsx
+++ src/services/context/settings.tsx
@@ -1,7 +1,7 @@
 import { lazy, Suspense, Fragment } from "react";
 import { BrowserRouter, Route, Switch } from "react-router-dom";
 import { ContextLoader } from "../../shared/loaders";
-import ProtectedRoutes from "../adapters/protectedRoutes";
+// import ProtectedRoutes from "../adapters/protectedRoutes";
 
 const ClientLoginComponent = lazy(
   () => import("../../pages/auth/client-login")
@@ -23,6 +23,7 @@ const LandingPageComponent = lazy(() => import("../../pages/landingpage"));
 
 // client dashboard
 const Dashboard = lazy(() => import("../../pages/client-dashboard/dashboard"));
+const History = lazy(() => import("../../pages/client-dashboard/history"));
 
 const SettingsConfig = () => {
   return (
@@ -60,11 +61,8 @@ const SettingsConfig = () => {
               path={"/client-registration"}
               exact={true}
             />
-            <ProtectedRoutes
-              component={Dashboard}
-              path={"/app/"}
-              exact={true}
-            />
+            <Route component={Dashboard} path={"/app/"} exact={true} />
+            <Route component={History} path={"/app/history"} exact={true} />
             <Route component={LandingPageComponent} path={"/"} />
           </Switch>
         </Suspense>
