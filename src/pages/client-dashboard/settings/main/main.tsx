ceb1c0402b0c07d506b29a97af1ad999fe1410a7
arkhurst <fiiarkhurst@gmail.com>
2021-07-25 12:06:06 +0000

    added settings screen

--- src/pages/client-dashboard/settings/main/main.tsx
+++ src/pages/client-dashboard/settings/main/main.tsx
@@ -4,7 +4,186 @@ const MainComponent = () => {
   return (
     <Fragment>
       <div className="max-w-7xl mx-auto flex justify-between items-center px-4 py-5 sm:px-6 sm:py-4 lg:px-8 md:justify-start md:space-x-10">
-        <div>hi settings</div>
+        {/* Main content */}
+        <div className="flex-1 max-h-screen xl:overflow-y-auto">
+          <div className="max-w-3xl mx-auto py-10 px-4 sm:px-6 lg:py-12 lg:px-8">
+            <h1 className="text-2xl font-bold text-gray-900">
+              Account Settings
+            </h1>
+
+            <form className="mt-6 space-y-8 divide-y divide-y-gray-200">
+              <div className="grid grid-cols-1 gap-y-6 sm:grid-cols-6 sm:gap-x-6">
+                <div className="sm:col-span-6">
+                  <h2 className="text-xl font-medium text-gray-900">Profile</h2>
+                  <p className="mt-1 text-sm text-gray-500">
+                    This information will be displayed publicly so be careful
+                    what you share.
+                  </p>
+                </div>
+
+                <div className="sm:col-span-3">
+                  <label
+                    htmlFor="first-name"
+                    className="block text-sm font-medium text-gray-900"
+                  >
+                    First name
+                  </label>
+                  <input
+                    type="text"
+                    required
+                    className="shadow-none font-light py-2 mt-2 px-2 bg-white border focus:outline-none block w-full sm:text-sm border-gray-300 rounded-md focus:ring-pink-600  focus:shadow-outline-purple focus:border-pink-600"
+                    placeholder="Eg. John"
+                  />
+                </div>
+
+                <div className="sm:col-span-3">
+                  <label
+                    htmlFor="last-name"
+                    className="block text-sm font-medium text-gray-900"
+                  >
+                    Last name
+                  </label>
+                  <input
+                    type="text"
+                    required
+                    className="shadow-none font-light py-2 mt-2 px-2 bg-white border focus:outline-none block w-full sm:text-sm border-gray-300 rounded-md focus:ring-pink-600  focus:shadow-outline-purple focus:border-pink-600"
+                    placeholder="Eg. Doe"
+                  />
+                </div>
+
+                <div className="sm:col-span-6">
+                  <label
+                    htmlFor="photo"
+                    className="block text-sm font-medium text-gray-900"
+                  >
+                    Photo
+                  </label>
+                  <div className="mt-1 flex items-center">
+                    <img
+                      className="inline-block h-14 w-14 rounded-full"
+                      src="https://images.unsplash.com/photo-1550525811-e5869dd03032?ixlib=rb-1.2.1&auto=format&fit=facearea&facepad=2.5&w=256&h=256&q=80"
+                      alt=""
+                    />
+                    <div className="ml-4 flex">
+                      <div className="relative bg-white py-2 px-3 border border-gray-300 rounded-md shadow-sm flex items-center cursor-pointer hover:bg-gray-50 focus-within:outline-none focus-within:ring-2 focus-within:ring-offset-2 focus-within:ring-offset-gray-50 focus-within:ring-blue-500">
+                        <label
+                          htmlFor="user-photo"
+                          className="relative text-sm font-medium text-gray-900 pointer-events-none"
+                        >
+                          <span>Change</span>
+                          <span className="sr-only"> user photo</span>
+                        </label>
+                        <input
+                          id="user-photo"
+                          name="user-photo"
+                          type="file"
+                          className="absolute inset-0 w-full h-full opacity-0 cursor-pointer border-gray-300 rounded-md"
+                        />
+                      </div>
+                      <button
+                        type="button"
+                        className="ml-3 bg-transparent py-2 px-3 border border-transparent rounded-md text-sm font-medium text-gray-900 hover:text-gray-700 focus:outline-none focus:border-gray-300 focus:ring-2 focus:ring-offset-2 focus:ring-offset-gray-50 focus:ring-blue-500"
+                      >
+                        Remove
+                      </button>
+                    </div>
+                  </div>
+                </div>
+
+                <div className="sm:col-span-3">
+                  <label
+                    htmlFor="email-address"
+                    className="block text-sm font-medium text-gray-900"
+                  >
+                    Email address
+                  </label>
+                  <input
+                    type="email"
+                    required
+                    className="shadow-none font-light py-2 mt-2 px-2 bg-white border focus:outline-none block w-full sm:text-sm border-gray-300 rounded-md focus:ring-pink-600  focus:shadow-outline-purple focus:border-pink-600"
+                    placeholder="Eg. john@gmail.com"
+                  />
+                </div>
+
+                <div className="sm:col-span-3">
+                  <label
+                    htmlFor="phone-number"
+                    className="block text-sm font-medium text-gray-900"
+                  >
+                    Phone number
+                  </label>
+                  <input
+                    type="number"
+                    required
+                    className="shadow-none font-light py-2 mt-2 px-2 bg-white border focus:outline-none block w-full sm:text-sm border-gray-300 rounded-md focus:ring-pink-600  focus:shadow-outline-purple focus:border-pink-600"
+                    placeholder="Eg. 0200000000"
+                  />
+                </div>
+              </div>
+
+              <div className="pt-8 grid grid-cols-1 gap-y-6 sm:grid-cols-6 sm:gap-x-6">
+                <div className="sm:col-span-6">
+                  <h2 className="text-xl font-medium text-gray-900">
+                    Account Security
+                  </h2>
+                </div>
+
+                <div className="sm:col-span-3">
+                  <label
+                    htmlFor="email-address"
+                    className="block text-sm font-medium text-gray-900"
+                  >
+                    Password
+                  </label>
+                  <input
+                    type="password"
+                    required
+                    className="shadow-none font-light py-2 mt-2 px-2 bg-white border focus:outline-none block w-full sm:text-sm border-gray-300 rounded-md focus:ring-pink-600  focus:shadow-outline-purple focus:border-pink-600"
+                    placeholder=""
+                  />
+                </div>
+
+                <div className="sm:col-span-3">
+                  <label
+                    htmlFor="phone-number"
+                    className="block text-sm font-medium text-gray-900"
+                  >
+                    Confirm Password
+                  </label>
+                  <input
+                    type="password"
+                    required
+                    className="shadow-none font-light py-2 mt-2 px-2 bg-white border focus:outline-none block w-full sm:text-sm border-gray-300 rounded-md focus:ring-pink-600  focus:shadow-outline-purple focus:border-pink-600"
+                    placeholder=""
+                  />
+                </div>
+
+                <p className="text-sm text-gray-500 sm:col-span-6">
+                  This account was created on{" "}
+                  <time dateTime="2017-01-05T20:35:40">
+                    January 5, 2017, 8:35:40 PM
+                  </time>
+                  .
+                </p>
+              </div>
+
+              <div className="pt-8 flex justify-end">
+                <button
+                  type="button"
+                  className="bg-white py-2 px-4 border border-gray-300 rounded-md shadow-sm text-sm font-medium text-gray-900 hover:bg-gray-50 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-pink-600"
+                >
+                  Cancel
+                </button>
+                <button
+                  type="submit"
+                  className="ml-3 inline-flex justify-center py-2 px-4 border border-transparent shadow-sm text-sm font-medium rounded-md text-white bg-pink-600 hover:bg-pink-500 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-pink-600"
+                >
+                  Save
+                </button>
+              </div>
+            </form>
+          </div>
+        </div>
       </div>
     </Fragment>
   );
