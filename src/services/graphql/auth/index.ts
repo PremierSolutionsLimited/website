c613b9ba794b1982faaa4fa565bddae1648aa544
arkhurst <fiiarkhurst@gmail.com>
2021-09-15 00:16:30 +0000

    update client details should work

--- src/services/graphql/auth/index.ts
+++ src/services/graphql/auth/index.ts
@@ -4,3 +4,4 @@ export { sendClientCode as SEND_CLIENT_CODE } from "./mutation";
 export { verifyClientCode as VERIFY_CLIENT_CODE } from "./mutation";
 export { resetClientCode as RESET_CLIENT_CODE } from "./mutation";
 export { currentClient as CURRENT_CLIENT } from "./query";
+export { updateCurrentClient as UPDATE_CURRENT_CLIENT } from "./mutation";
