1211c5b29187d54e5edbe3cc4d6aa343a9266775
kdgyekye <kingsleygyeky@gmail.com>
2022-04-14 12:12:11 +0000

    changes

--- src/pages/landingpage/components/faqs.tsx
+++ src/pages/landingpage/components/faqs.tsx
@@ -8,7 +8,7 @@ const FrequentlyAskedQuestions = () => {
           <div className="container px-5 py-24 mx-auto">
             <div className="text-center mb-20">
               <h1 className="sm:text-3xl text-2xl font-medium text-center title-font text-gray-900 mb-4">
-                Frequently Asked Question
+                Frequently Asked Questions
               </h1>
               <p className="text-base leading-relaxed xl:w-2/4 lg:w-3/4 mx-auto">
                 The most common questions about how our business works and what
