e40b65fef4a25cfc78f958de13069a4ae4398343
kdgyekye <kingsleygyeky@gmail.com>
2022-06-06 17:16:31 +0000

    Minor changes

--- src/pages/client-dashboard/history/data-view/main.tsx
+++ src/pages/client-dashboard/history/data-view/main.tsx
@@ -57,7 +57,7 @@ const DataViewComponent: React.FC<HistoryDataViewComponentProp> = ({
                     scope="col"
                     className="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider"
                   >
-                    Expected Start Time
+                    Start Time
                   </th>
                   {/* <th
                     scope="col"
