8abe180c3f2f5d9aa4f9a9f602516dd8b50205df
kdgyekye <kingsleygyeky@gmail.com>
2022-06-16 22:52:50 +0000

    changed colors of loaders

--- src/shared/loaders/listLoaders.tsx
+++ src/shared/loaders/listLoaders.tsx
@@ -4,7 +4,7 @@ import { ClassicSpinner } from "react-spinners-kit";
 const ListLoader = () => {
   return (
     <Fragment>
-      <ClassicSpinner color={"#d81b60"} size={15} loading={true} />
+      <ClassicSpinner color={"#CC8A00"} size={15} loading={true} />
     </Fragment>
   );
 };
